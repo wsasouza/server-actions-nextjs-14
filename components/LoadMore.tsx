e5481c202141331f00d1aee65ee37b42c09c310b
Walter Santos de Andrade Souza <wsasouza@hotmail.com>
2023-11-25 19:50:57 -0300

    Feat: Finihed App

--- components/LoadMore.tsx
+++ components/LoadMore.tsx
@@ -1,10 +1,35 @@
-import Image from "next/image";
+'use client';
+
+import { useEffect, useState } from 'react';
+import Image from 'next/image';
+import { useInView } from 'react-intersection-observer';
+import { fetchAnime } from '@/app/action';
+import AnimeCard from './AnimeCard';
+
+let page = 2;
+
+export type AnimeCard = JSX.Element;
 
 function LoadMore() {
+  const [data, setData] = useState<AnimeCard[]>([]);
+  const { ref, inView } = useInView();
+
+  useEffect(() => {
+    if (inView) {
+      fetchAnime(page).then((res) => {
+        setData([...data, ...res]);
+      });
+      page++;
+    }
+  }, [data, inView]);
+
   return (
     <>
+      <section className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-10">
+        {data}
+      </section>
       <section className="flex justify-center items-center w-full">
-        <div>
+        <div ref={ref}>
           <Image
             src="./spinner.svg"
             alt="spinner"
